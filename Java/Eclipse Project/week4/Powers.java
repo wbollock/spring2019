7580159f9ca13132faf598ceba45292dc35ca5d7
Will Bollock <wbollock@gmail.com>
2019-02-04 17:08:09 -0500

    week 4 homework, project homework

--- Java/Eclipse Project/week4/Powers.java
+++ Java/Eclipse Project/week4/Powers.java
@@ -5,7 +5,6 @@ public class Powers {
  * Author: Will Bollock, web15c
  */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		// print all powers of 2 for an upper range and calculate their sum
 		
@@ -20,33 +19,28 @@ public class Powers {
 		// print all powers of 2 for an upper range and calculate their sum
 		
 		
-		
 		int power=2;
-		System.out.print("1"); // 1 will always be a factor
+		System.out.print("1"); // 1 will always be a factor, print 1 here so "+" sign is only on previous values
 		while(power <= upperLimit)
 		{
 			
-			
-				System.out.print(" + " + power);
+				System.out.print(" + " + power); // + sign first so it won't hang at the end
 				power = power * 2; // power of 2
-				sum = sum + power;
+				sum = sum + power; // calculate sum
 				
-					/*if((sum/2) == (2 * power - 1)) // the last value
+					/*if((sum/2) == (2 * power - 1)) // for debug purposes
 					{
 					//System.out.print(+);
 					System.out.print("test");
 					//break; // leave while loop
-					}*/
-					
-				
-				// for last value, need to get rid of the plus sign
-				//debug
-				//System.out.print("(SUM = " + sum + ")" + power);
+					}*/ 
+					//System.out.print("(SUM = " + sum + ")" + power);
 				
 				
 		} // end of while loop
-			System.out.print(" = " + (sum/2)); // divide by two to get correct value
-			// one solution is to add /b/b to this print statement.. not good
+		
+		System.out.print(" = " + (sum/2 + 1)); // divide by two to get correct value, add 1
+		
 	}
 	
 
--- Java/Eclipse Project/week4/Primes.java
+++ Java/Eclipse Project/week4/Primes.java
@@ -5,33 +5,32 @@ public class Primes {
  * Author: Will Bollock, web15c
  */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int upbound;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the upper limit: ");
 		upbound = sc.nextInt();
 		
-		//iterate through upperLimit, print prime numbers
+		//iterate through uppbound, print prime numbers
 		System.out.println("The prime numbers are: ");
 		System.out.print("2"); // for formatting, 2 will always be prime
 		for(int i = 3; i <= upbound ; i++) // counter var #1. also first prime number has to be 3 at least
 		{
+			int extravar=0; // flag var
 			
-			int extravar=0; // a third counter var
-			for(int n = 3; n <= i/2; n++) // n has to be at least least than i/2, because it won't divide evenly
+			for(int n = 2; n <= i/2; n++) // n has to be at least least than i/2, because it won't divide evenly
 			{
-				if(i % n == 0) // checking to see if it's NOT prime
+				if(i % n == 0) // if it's not prime, then add to extravar
 				{
-				extravar++;
-					// if it's not prime, then add to extravar
-				}	
-			}
+				extravar++; // flag
+				}	 // end of if
+				
+			} // end of inner for
 		if(extravar == 0) // if i added to extravar, this can't be true, so it wouldn't print
-					
 					System.out.print( ", " + i); // should be noted for myself this is outside the inner loop
-		}
-		System.out.println(""); // moved outside the loops
+		} // end of outer for
+		
 	}
 	
 }
