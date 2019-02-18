c66496345b5340e18977aec90afbda33dc9015ef
Will Bollock <wbollock@gmail.com>
2019-02-18 14:09:35 -0500

    start of size function, getting user input

---
+++ Web_Design/A3/js/scripts.js
@@ -0,0 +1,25 @@
+// made by William Bollock, CGS 3066 A3
+
+// how do you call a function if the html is read only??
+
+// Change Size function
+//if button pressed, then...
+document.getElementById('size_btn').onclick = function() {
+    // window.alert("button was clicked"); (DEBUG TODO)
+    
+    // get inputs for width and height
+    // https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
+    document.getElementByID('')
+
+    // if user input height == X
+    //then...
+    var heightVar;
+    document.getElementById("result").style.height = "heightVar" + "px";
+
+    //width
+    var widthVar;
+    document.getElementById("result").style.width = "widthVar" + "px";
+
+    
+
+};
