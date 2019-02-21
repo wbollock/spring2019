04d511947f7e417ca9ed2963996dea1c75ee5a69
Will Bollock <wbollock@gmail.com>
2019-02-21 11:33:53 -0500

    working on adding more divs

--- Web_Design/A3/js/scripts.js
+++ Web_Design/A3/js/scripts.js
@@ -9,13 +9,13 @@ TODO: USE EVENT LISTENERS(?) -> see line 116 of JavaScript5.org
 */
 
 // Change Size function
-function size_function() {
+function size () {
     // get inputs for width and height
-    var heightVar = document.querySelector("#height").value;
-    var heightInt = parseInt(heightVar);
+    let heightVar = document.querySelector("#height").value;
+    let heightInt = parseInt(heightVar);
 
-    var widthVar = document.querySelector("#width").value;
-    var widthInt = parseInt(widthVar);
+    let widthVar = document.querySelector("#width").value;
+    let widthInt = parseInt(widthVar);
     // if user input height == X
     //then...
 
@@ -26,23 +26,45 @@ function size_function() {
 
 
 //Change Colors, user selects colors, "Change Colors" button will adjust row(s) to select colored
-function color_function () {
+function color () {
     // need to be able to get the colors in an array of sort?
     // maybe a loop could work here
     // id's go from color_sel0 ++
     //maybe a for loop (loop for 5, 1-4, etc)
     // color_sel0 = first color
     // color_sel1 = second color
+    // TODO: account for negative numbers
 
-    var colorHex = document.querySelector("#color_sel0").value;
-    console.log(colorHex);
+   
 
+    const numColors = parseInt(document.querySelector("#num_colors").value);
+    let new_div= new Array();
+
+    for(let i=0; i<Math.abs(numColors); i++) // sets colors to row
+    {
+        let colorHex = document.querySelector("#color_sel" + i).value; // succesfully prints subsequent values
+        console.log(colorHex); //TODO: DELETE. FOR DEBUG
+        let colorArray = document.getElementsByClassName("result_row");
+
+        // need to set colorHex to result row(s)
+        colorArray[0].style.backgroundColor = colorHex;
+        // need to make more than one div
+        new_div[i] = document.createElement('div'); // create div
+        document.body.appendChild(new_div[i]); // put the div in the body
+        new_div[i].style.backgroundColor = colorHex;
+        new_div[i].style.height = 100/(i+1);
+        console.log(i);
+
+        // depending on number of colors, make more rows
+       //new_div += '<div class="result_row style="background-color:' + colorAr
+        
+    }
     //document.querySelectorAll("div.result_row").style.backgroundColor = 'colorHex';
-    var colorArray = document.getElementsByClassName("result_row");
-    colorArray[0].style.backgroundColor = colorHex;
+    //good code below
+    //let colorArray = document.getElementsByClassName("result_row");
+    //colorArray[0].style.backgroundColor = colorHex;
     
-    var colorHex1 = document.querySelector("#color_sel1").value;
-    console.log(colorHex1);
+
 
     // if colorHex1.value !== null, then create another row and apply the next color?
    
@@ -54,18 +76,7 @@ function color_function () {
 }
 
 // calling functions
-document.querySelector('#size_btn').addEventListener("click", size_function);
+document.querySelector('#size_btn').addEventListener("click", size);
 
-document.querySelector('#colors_btn').addEventListener("click", color_function);
+document.querySelector('#colors_btn').addEventListener("click", color);
 
-
-
-
-/*
-function onClick() {
-   console.log('clicked');
-}
-
-const button = document.querySelector('button');
-button.addEventListener('click', onClick);
-*/
