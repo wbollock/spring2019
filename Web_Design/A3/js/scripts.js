060e4022cc5a2ac490ae62c2209efdec13d7a1cc
Will Bollock <wbollock@gmail.com>
2019-02-21 10:17:01 -0500

    changed to addEventListeners

--- Web_Design/A3/js/scripts.js
+++ Web_Design/A3/js/scripts.js
@@ -4,15 +4,17 @@
 the input entered by the user), first get a reference to the element using a querySelector, and
 then use the attribute/field value. Remember that such value is always returned as a string.
 TODO: DELETE ABOVE
+TODO: CHANGE VARS TO LETS/CONSTS
+TODO: USE EVENT LISTENERS(?) -> see line 116 of JavaScript5.org
 */
 
 // Change Size function
-document.getElementById('size_btn').onclick = function () { // may not 
+function size_function() {
     // get inputs for width and height
     var heightVar = document.querySelector("#height").value;
     var heightInt = parseInt(heightVar);
 
-    var widthVar = document.getElementById("width").value;
+    var widthVar = document.querySelector("#width").value;
     var widthInt = parseInt(widthVar);
     // if user input height == X
     //then...
@@ -23,9 +25,8 @@ document.getElementById('size_btn').onclick = function () { // may not
 };
 
 
-//Change Colors
-// user selects colors, "Change Colors" button will adjust row(s) to select colored
-document.getElementById('colors_btn').onclick = function () {
+//Change Colors, user selects colors, "Change Colors" button will adjust row(s) to select colored
+function color_function () {
     // need to be able to get the colors in an array of sort?
     // maybe a loop could work here
     // id's go from color_sel0 ++
@@ -45,7 +46,26 @@ document.getElementById('colors_btn').onclick = function () {
 
     // if colorHex1.value !== null, then create another row and apply the next color?
    
+    // might want to use an eventlistener OR innerHTML w/ a loop to add rows and stuff
 
+
+   
 
 }
 
+// calling functions
+document.querySelector('#size_btn').addEventListener("click", size_function);
+
+document.querySelector('#colors_btn').addEventListener("click", color_function);
+
+
+
+
+/*
+function onClick() {
+   console.log('clicked');
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onClick);
+*/
