2390cd3e32cbb2923f56f63f8a825e1c5d98a91f
Will Bollock <wbollock@gmail.com>
2019-02-26 00:24:32 -0500

    finished formatting for primes

--- Java/Eclipse Project/src/homework4/Primes.java
+++ Java/Eclipse Project/src/homework4/Primes.java
@@ -29,10 +29,17 @@ public class Primes {
 		// find and print all prime numbers less than or equal to user input
 		// restrict output to 9 numbers per line
 		System.out.println("The prime numbers less than or equal to " + userInt + " are:");
+		int counter=0;
 		for(int i = 2; i <= userInt ; i++) {
 			if(isPrime(i) == true)
 			{
 				System.out.print(i + " ");
+				counter++; // if counter gets to 9, print new line
+				if(counter == 9)
+				{
+					System.out.println("");
+					counter = 0; // reset counter
+				}
 			}
 		}
 
