f97217d59a21a10735c090f9bd2aac0577add5db
Will Bollock <wbollock@gmail.com>
2019-03-13 18:29:56 -0400

    added username

--- Web_Design/A4/js/scripts.js
+++ Web_Design/A4/js/scripts.js
@@ -1,11 +1,11 @@
 /* made by Will Bollock, for CGS 3066 */
 // TODO: get rid of Console.logs()
+// TODO: if it fails even JUST 1 VALIDATON RULE, cannot pass data to form and change output
 function isNameValid() {
     // name must have first letter in uppercase
     // name must also be filled out
     //let nameVar = document.forms["main-form"]["name"].value;
     let nameVar = document.getElementById("main-form").name.value;
-    console.log("This is NAMEVAR: " + nameVar);
     let new_output = "";
     let userNameHTML = "<span style=\"color:black\">" + "Your name is:" + "</span>";
     if(nameVar == "") 
@@ -18,8 +18,6 @@ function isNameValid() {
     else if(nameVar[0] == nameVar[0].toUpperCase() && nameVar !== "") // if first char uppercase and not empty
     {
         new_output += "<div id=\"output\">" + userNameHTML + nameVar + "</div>";
-        console.log("New output is: "  + new_output);
-        console.log("nameValid true Uppcase");
         document.getElementById("output").innerHTML = new_output;
     }
     else // if first letter of word is not capital
@@ -28,16 +26,40 @@ function isNameValid() {
         failDiv = "<p style=\"color:red\">The first letter of your name must be capitalized.</p>";
         let rowArray = document.querySelectorAll(".row"); // first row, which is name
         rowArray[0].innerHTML += failDiv; // manipulate the first row
-        console.log("Add fail div");
-        
     }
-    
-    
-    
+} // end of functon
 
+function isUsernameValid() {
+    // must be 5 characters long
+    let usernameVar = document.getElementById("main-form").username.value;
+    let new_output = ""; 
+    // TODO: usernameHTML needs to be added below previous outputs
+    let usernameHTML = "<span style=\"color:black\">" + "Your username is:" + "</span>";
+    console.log("usernameVar is: " + usernameVar);
+    if(usernameVar == "")  // cannot be empty
+    { 
+        let failDivEmpty = document.createElement("div");
+        failDivEmpty = "<p style=\"color:red\">Your username is empty.</p>";
+        let rowArray = document.querySelectorAll(".row"); // username row
+        rowArray[1].innerHTML += failDivEmpty; // manipulate the row
+        console.log("Username empty");
+    }
+    else if(usernameVar.length > 4 && usernameVar !== "") 
+    // if username is not 5 characters long and not empty
+    {
+        new_output += "<div id=\"output\">" + usernameHTML + usernameVar + "</div>";
+        document.getElementById("output").innerHTML += new_output; // += takes out output and adds new output
+        console.log("Username good");
+    }
+    else // if username is not 5 characters
+    {
+        let failDiv = document.createElement("div");
+        failDiv = "<p style=\"color:red\">Your username must be 5 characters long</p>";
+        let rowArray = document.querySelectorAll(".row"); 
+        console.log("Username not 5 chars");
+
+    }
 }
-
-
 
 
 
@@ -68,6 +90,7 @@ function isNameValid() {
 
 
 document.querySelector("#submit-btn").addEventListener("click", isNameValid);
+document.querySelector("#submit-btn").addEventListener("click", isUsernameValid);
 
 
 
