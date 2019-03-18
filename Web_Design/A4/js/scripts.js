9acbd20e0e53c622ad889462e4b5939e8167c456
Will Bollock <wbollock@gmail.com>
2019-03-18 15:56:36 -0400

    password done

--- Web_Design/A4/js/scripts.js
+++ Web_Design/A4/js/scripts.js
@@ -1,11 +1,15 @@
 /* made by Will Bollock, for CGS 3066 */
+
 // TODO: get rid of Console.logs()
-// TODO: if it fails even JUST 1 VALIDATON RULE, cannot pass data to form and change output
-// TODO: bug -> "Your string is empty" appears after validation is false? Also happens w/ username
+//if it fails even JUST 1 VALIDATON RULE, cannot pass data to form and change output
+/*
+2a. EC: tell USer what went wrong as SOON as they enter data (possibly call function when field is in focus?)
+3. Fix bug in code allowing user to select Maloe and other gender at the same time
+*/
+
 function isNameValid() {
     // name must have first letter in uppercase
     // name must also be filled out
-    //let nameVar = document.forms["main-form"]["name"].value;
     let nameVar = document.getElementById("main-form").name.value;
     let new_output = "";
     let userNameHTML = "<span style=\"color:black\">" + "Your name is:" + "</span>";
@@ -26,9 +30,9 @@ function isNameValid() {
     else // if first letter of word is not capital
     {
         let failDiv = document.createElement("div");
-        failDiv = "<p style=\"color:red\">The first letter of your name must be capitalized.</p>";
-        let rowArray = document.querySelectorAll(".row"); // first row, which is name
-        rowArray[0].innerHTML += failDiv; // manipulate the first row
+        failDiv = "<p style=\"color:red\">The first letter of your name must be capitalized.</p>"; // for uer
+        let rowArray = document.querySelectorAll(".row"); 
+        rowArray[0].innerHTML += failDiv; 
         return 2; // meaning validation failed, don't pass anything to result
     }
 } // end of functon
@@ -37,16 +41,13 @@ function isUsernameValid() {
     // must be 5 characters long
     let usernameVar = document.getElementById("main-form").username.value;
     let new_output = ""; 
-    // TODO: usernameHTML needs to be added below previous outputs
     let usernameHTML = "<span style=\"color:black\">" + "Your username is:" + "</span>";
-    console.log("usernameVar is: " + usernameVar);
     if(usernameVar == "")  // cannot be empty
     { 
         let failDivEmpty = document.createElement("div");
         failDivEmpty = "<p style=\"color:red\">Your username is empty.</p>";
         let rowArray = document.querySelectorAll(".row"); // username row
         rowArray[1].innerHTML += failDivEmpty; // manipulate the row
-        console.log("Username empty");
         //return false; // meaning validation failed, don't pass anything to result
         return 2;
     }
@@ -55,50 +56,51 @@ function isUsernameValid() {
     {
         new_output += "<div id=\"output\">" + usernameHTML + usernameVar + "</div>";
         document.getElementById("output").innerHTML += new_output; // += takes out output and adds new output
-        console.log("Username good");
-        return 1;
+        return 1; 
     }
     else // if username is not 5 characters
     {
         let failDiv = document.createElement("div");
         failDiv = "<p style=\"color:red\">Your username must be 5 characters long</p>";
-        let rowArray = document.querySelectorAll(".row"); 
-        console.log("Username not 5 chars");
+        let rowArray = document.querySelectorAll(".row");
         rowArray[1].innerHTML += failDiv; // manipulate the first row
         return 2; // meaning validation failed, don't pass anything to result
 
     }
 }
+function isPasswordValid() {
+    // must be 6 characters long
+    let passwordVar = document.getElementById("main-form").password.value;
+    let new_output = ""; 
+    let passwordHTML = "<span style=\"color:black\">" + "Your password has:" + "</span>";
+    if(passwordVar == "")  // cannot be empty
+    { 
+        let failDivEmpty = document.createElement("div");
+        failDivEmpty = "<p style=\"color:red\">Your password is empty.</p>";
+        let rowArray = document.querySelectorAll(".row"); // password row
+        rowArray[2].innerHTML += failDivEmpty; // manipulate the row
+        //return false; // meaning validation failed, don't pass anything to result
+        return 2;
+    }
+    else if(passwordVar.length > 5 && passwordVar !== "") 
+    // if password is 6 characters long and not empty (good)
+    {
+        // replace password contents with password length
+        passwordLen = passwordVar.length;
+        new_output += "<div id=\"output\">" + passwordHTML + passwordLen + " characters" + "</div>";
+        document.getElementById("output").innerHTML += new_output; // += takes out output and adds new output
+        return 1; 
+    }
+    else // if password is not 6 characters
+    {
+        let failDiv = document.createElement("div");
+        failDiv = "<p style=\"color:red\">Your password must be 6 characters long</p>";
+        let rowArray = document.querySelectorAll(".row");
+        rowArray[2].innerHTML += failDiv; 
+        return 2; 
 
-
-
-    
-
-// 1. Result column needs to display information PASSED BY USER if data is VALID
-// need to get rid of "form not submitted yet " id=output
-
-// 2. Perform data validation on info passed by USER, inform user of what went wrong - on the very field that's wrong
-// (use innerHTML to create a row underneath the form line)
-// 2a. EC: tell USer what went wrong as SOON as they enter data (possibly call function when field is in focus?)
-
-
-// 3. Fix bug in code allowing user to select Maloe and other gender at the same time
-
-
-
-// maybe want functions for valid submission, e.g isValidName() == true? if true, pass values
-
-
-
-
-
-
-
-
-
-//document.querySelector("#submit-btn").addEventListener("click", isNameValid);
-//document.querySelector("#submit-btn").addEventListener("click", isUsernameValid);
-
+    }
+}
 
    
 
@@ -108,11 +110,11 @@ document.querySelector("#submit-btn").addEventListener("click", function(event){
 
     let nameValidValue = isNameValid();
     let userNameValue = isUsernameValid();
+    let passwordValue = isPasswordValid();
 
-    if(nameValidValue === 2 || userNameValue === 2)
+    if(nameValidValue === 2 || userNameValue === 2 || passwordValue == 2)
     {
         // rewrite result to default
-        console.log("DEBUG: REWRITING OUTPUT");
        document.getElementById("output").innerHTML = "<div id=\"output\"> Form not submitted yet </div>";
     }
 
